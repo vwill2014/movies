0249f04fa83f988ae0785f6009f8cf97ac71be21
Veronica Williams <vcwilliams2014@gmail.com>
2017-12-10 22:50:27 -0800

    Uploading Files

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO films(name) VALUES('Marshall');
+INSERT INTO films(name) VALUES('Wonder');
+INSERT INTO films(name) VALUES('Black Panther');
+INSERT INTO films(name) VALUES('Jurassic World');
+INSERT INTO films(name) VALUES('Bright');
